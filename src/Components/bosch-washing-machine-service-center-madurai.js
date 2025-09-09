077c0579e8b8614419c1f699afdc8e86f38568a8
E2ewebserv <e2ewebservice@gmail.com>
2025-09-09 16:04:49 +0530

    Update lavanya bosch-washing-machine-service-center-madurai.js

--- src/Components/bosch-washing-machine-service-center-madurai.js
+++ src/Components/bosch-washing-machine-service-center-madurai.js
@@ -12,6 +12,8 @@ import WHATSAPPPNG from '../images/WHATSAPPPNG.png'
 import WASHSIT from '../images/WASHSIT.jpg'
 import WASHDESC from '../images/WASHDESC.webp'
 import { Helmet } from "react-helmet";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // import BOSCHWASHINGMACHINESERVICE from './bosch-washing-machine-service'
 
 
@@ -172,7 +174,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
                 <Navbar />
               </div>
         
-        <section id="headline">
+        {/* <section id="headline">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -180,19 +182,42 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
                   </div>
                 </div>
               </div>
-            </section>
-        
+            </section> */}
+        <section className="highlight-strip">
+  <div className="scroll-container">
+    <div className="scroll-track">
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  </div>
+</section>
               <section id="hero" style={{ backgroundImage: `url(${WASHERHERO})` }}>
                   <div className="hero-content">
                      
-                    <h1>WE ARE READY TO HELP</h1>
+                    <h1>BEST BOSCH WASHING MACHINE SERVICE CENTER MADURAI</h1>
                     <h3>24*7 Service Support</h3>
                     
                     <a href="tel:+919025312399" className="hero-btn">+919025312399</a>
                   </div>
                 </section>
         
-        <section id="cchl">
+        <section id="cchl" data-aos="fade-right">
           <div className='container'>
     <div className='row'>
     <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -206,7 +231,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
           </div>
           </section> 
    
-          <section id="start">
+          <section id="start" data-aos="fade-left">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
@@ -220,7 +245,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
       </div>
     </section>
     
-    <section id="CEE">
+    <section id="CEE" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
@@ -231,7 +256,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Email Us</h3>
             <img src={EMAILPNG} id="EMAILPNG" />
-            <a href="#">BOOK NOW</a>
+             <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a>
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Enquiry Us</h3>
@@ -247,7 +272,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -260,12 +285,12 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </section>
     
     
-    <section id="LIST">
+    <section id="LIST" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
     
-            <h2>BoschCustomer Care Helpline Support</h2>
+            <h2 data-aos="fade-left">BoschCustomer Care Helpline Support</h2>
             <ul>
            <li> <i class='fas fa-arrow-circle-right'></i>Book Your Complaints 24*7 Customer Care</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Quick Service Within 2 Hours</li>
@@ -275,7 +300,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
               <li> <i class='fas fa-arrow-circle-right'></i>Trusted Certified Technicians</  li>
             </ul>
     
-            <h2>Issues In Bosch washing Machine</h2>
+            <h2 data-aos="fade-left">Issues In Bosch washing Machine</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Washing Machine On-Off Problem</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Noise in The bosch washing Machine</li>
@@ -286,7 +311,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
               <li> <i class='fas fa-arrow-circle-right'></i>Door Not Lock Problem</li>
             </ul>
     
-            <h2>Types Of Bosch washing Machine Service</h2>
+            <h2 data-aos="fade-left">Types Of Bosch washing Machine Service</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Top Load bosch washing Machine Service</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Front Load bosch washing Machine Service</li>
@@ -299,7 +324,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </section>
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -337,7 +362,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </div>
     </div>
     </section> */}
-    <section id="STARS">
+    <section id="STARS" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
          
@@ -376,7 +401,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -389,7 +414,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </section>
     
     
-    <section id="RATING">
+    <section id="RATING" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-3 col-md-3 col-sm-12 col-12' id="borders">
@@ -504,7 +529,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-    <section id="ours">
+    <section id="ours" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -520,7 +545,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-    <section id="DESCWASH">
+    <section id="DESCWASH" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-6 col-md-6 col-sm-12 col-12'>
@@ -548,7 +573,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </section>
     
     
-    <section id="call-btn">
+    <section id="call-btn" data-aos="fade-left">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -563,7 +588,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-            <section id="WWO">
+            <section id="WWO" data-aos="fade-right">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -580,7 +605,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
               </div>
             </section>
     
-            <section id="call-btn">
+            <section id="call-btn" data-aos="fade-left">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -594,7 +619,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
 
 
                                                        
-<section id='BDC'>
+<section id='BDC' data-aos="fade-right">
   <nav aria-label="breadcrumb" class="breadcrumb-container">
     <ol class="breadcrumb">
         <li><Link to="/">Home</Link></li>
@@ -607,7 +632,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
 
 
 
-<section id='SDS'>
+<section id='SDS' data-aos="fade-left">
   <div className="container">
     <h2 className="sds-title">Bosch Washing Machine Service – Complete Details</h2>
     <div className="sds-content">
@@ -617,7 +642,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
       <p><strong>Service Area:</strong> Madurai & Surrounding 20 km</p>
       <p><strong>Booking Mode:</strong> Online / Phone Call</p>
       <p><strong>Contact Number:</strong> <a href="tel:9025312399">9025312399</a></p>
-      <p><strong>Email:</strong> <a href="mailto:e2ewebservice@gmail.com">e2ewebservice@gmail.com</a></p>
+      <p><strong>Email:</strong>  <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a></p>
       <p><strong>Website:</strong> <a href="https://servicee2e.in">https://servicee2e.in</a></p>
       <p><strong>Working Hours:</strong> 9 AM – 9 PM (All Days)</p>
       <p><strong>Home Visit:</strong> Yes, Doorstep Service Available</p>
@@ -632,7 +657,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
 
 
 
-<section id='faqs'>
+<section id='faqs' data-aos="fade-right">
   <div className="container">
     <h2 className="faq-title">Bosch Washing Machine Service – FAQs</h2>
     <table className="faq-table">
@@ -691,7 +716,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
 
 
 
-<section id='UST'>
+<section id='UST' data-aos="fade-left">
 <div className='container'>
 <div className='row'>
 <div className='col-lg-12 col-md-12 col-12 col-sm-12'>
@@ -727,7 +752,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
 
     
     
-    <section id="interl">
+    <section id="interl" data-aos="fade-right">
     <div className='container'>
     <div className='row'>
       <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -755,7 +780,7 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     
     
     
-    <section id="FW">
+    <section id="FW" data-aos="fade-left">
         <div className="icon-container">
           <div className="icon facebook">
             <a
@@ -996,24 +1021,24 @@ export default function BOSCHWASHINGMACHINESERVICECENTERMADURAI() {
     </section> */}
     
     
-      <section id="TABOFCONTENT">
+      <section id="TABOFCONTENT" data-aos="fade-right">
       <div className='container'>
         <h2>Table of Contents</h2>
         <ul id="TAB">
-          <li><a href="#headline">1. Bosch washing Machine Service Customer Care</a></li>
-          <li><a href="#hero">2. Hero Section</a></li>
-          <li><a href="#cchl">3. Customer Care Helpline</a></li>
-          <li><a href="#CEE">4. Contact Us</a></li>
-          <li><a href="#WRHY">5. We Are Ready to Help You</a></li>
-          <li><a href="#LIST">6. Customer Care Helpline Support</a></li>
-          <li><a href="#WASHMANSIT">7. Why Choose Us?</a></li>
-          <li><a href="#STARS">8. Customer Support Ratings</a></li>
-          <li><a href="#RATING">9. Customer Testimonials</a></li>
-          <li><a href="#ours">10. Our Service</a></li>
-          <li><a href="#DESCWASH">11. bosch washing Machine Customer Support Service</a></li>
-          <li><a href="#WWO">12. What We Offer</a></li>
-          <li><a href="#interl">13. Related Services</a></li>
-          <li><a href="#FW">14. Follow Us</a></li>
+          {/* <li><a href="#headline">1. Bosch washing Machine Service Customer Care</a></li> */}
+          <li><a href="#hero">1. Hero Section</a></li>
+          <li><a href="#cchl">2. Customer Care Helpline</a></li>
+          <li><a href="#CEE">3. Contact Us</a></li>
+          <li><a href="#WRHY">4. We Are Ready to Help You</a></li>
+          <li><a href="#LIST">5. Customer Care Helpline Support</a></li>
+          <li><a href="#WASHMANSIT">6. Why Choose Us?</a></li>
+          <li><a href="#STARS">7. Customer Support Ratings</a></li>
+          <li><a href="#RATING">8. Customer Testimonials</a></li>
+          <li><a href="#ours">9. Our Service</a></li>
+          <li><a href="#DESCWASH">10. bosch washing Machine Customer Support Service</a></li>
+          <li><a href="#WWO">11. What We Offer</a></li>
+          <li><a href="#interl">12. Related Services</a></li>
+          <li><a href="#FW">13. Follow Us</a></li>
         </ul>
       </div>
     </section>
