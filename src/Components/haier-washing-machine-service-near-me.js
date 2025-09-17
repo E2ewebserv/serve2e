9a90e38a0a990e9923ee3889b11f8d2ef0fa3a7a
E2ewebserv <e2ewebservice@gmail.com>
2025-09-17 17:22:24 +0530

    Update lavanya haier-washing-machine-service-near-me.js

--- src/Components/haier-washing-machine-service-near-me.js
+++ src/Components/haier-washing-machine-service-near-me.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Header&Footer/Navbar';
-import Footer from '../Header&Footer/Footer';
+import Footers from '../Header&Footer/Footers';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
@@ -13,7 +13,8 @@ import WASHSIT from '../images/WASHSIT.jpg'
 import WASHDESC from '../images/WASHDESC.webp'
 import { Helmet } from "react-helmet";
 // import HAIERWASHINGMACHINESERVICE from './haier-washing-machine-service'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function HAIERWASHINGMACHINESERVICENEARME() {
     useEffect(() => {
@@ -175,7 +176,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
                 <Navbar />
               </div>
         
-        <section id="headline">
+        {/* <section id="headline">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -184,18 +185,42 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
                 </div>
               </div>
             </section>
-        
+         */}
+         <section className="highlight-strip">
+  <div className="scroll-container">
+    <div className="scroll-track">
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  </div>
+</section>
               <section id="hero" style={{ backgroundImage: `url(${WASHERHERO})` }}>
                   <div className="hero-content">
                      
-                    <h1>WE ARE READY TO HELP</h1>
+                    <h1>HAIER WASHING MACHINE SERVICE NEAR ME</h1>
                     <h3>24*7 Service Support</h3>
                     
                     <a href="tel:+919025312399" className="hero-btn">+919025312399</a>
                   </div>
                 </section>
         
-        <section id="cchl">
+        <section id="cchl" data-aos="fade-left">
           <div className='container'>
     <div className='row'>
     <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -209,7 +234,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
           </div>
           </section> 
    
-          <section id="start">
+          <section id="start" data-aos="fade-right">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
@@ -223,7 +248,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
       </div>
     </section>
     
-    <section id="CEE">
+    <section id="CEE" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
@@ -234,7 +259,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Email Us</h3>
             <img src={EMAILPNG} id="EMAILPNG" />
-            <a href="#">BOOK NOW</a>
+            <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a>
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Enquiry Us</h3>
@@ -250,7 +275,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -263,12 +288,12 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </section>
     
     
-    <section id="LIST">
+    <section id="LIST" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
     
-            <h2> Haier Customer Care Helpline Support</h2>
+            <h2 data-aos="fade-right"> Haier Customer Care Helpline Support</h2>
             <ul>
            <li> <i class='fas fa-arrow-circle-right'></i>Book Your Complaints 24*7 Customer Care</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Quick Service Within 2 Hours</li>
@@ -278,7 +303,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
               <li> <i class='fas fa-arrow-circle-right'></i>Trusted Certified Technicians</  li>
             </ul>
     
-            <h2>Issues In Haier washing Machine</h2>
+            <h2 data-aos="fade-right">Issues In Haier washing Machine</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Washing Machine On-Off Problem</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Noise in The haier washing Machine</li>
@@ -289,7 +314,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
               <li> <i class='fas fa-arrow-circle-right'></i>Door Not Lock Problem</li>
             </ul>
     
-            <h2>Types Of Haier washing Machine Service</h2>
+            <h2 data-aos="fade-right">Types Of Haier washing Machine Service</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Top Load haier washing Machine Service</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Front Load haier washing Machine Service</li>
@@ -302,7 +327,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </section>
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -313,7 +338,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </div>
     </div>
     </section>
-    <section id="WASHMANSIT">
+    <section id="WASHMANSIT" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -340,7 +365,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </div>
     </div>
     </section> */}
-    <section id="STARS">
+    <section id="STARS" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
          
@@ -379,7 +404,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -392,7 +417,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </section>
     
     
-    <section id="RATING">
+    <section id="RATING" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-3 col-md-3 col-sm-12 col-12' id="borders">
@@ -511,7 +536,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="ours">
+    <section id="ours" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -526,7 +551,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="DESCWASH">
+    <section id="DESCWASH" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-6 col-md-6 col-sm-12 col-12'>
@@ -554,7 +579,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </section>
     
     
-    <section id="call-btn">
+    <section id="call-btn" data-aos="fade-right">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -569,7 +594,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-            <section id="WWO">
+            <section id="WWO" data-aos="fade-left">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -584,7 +609,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
               </div>
             </section>
     
-            <section id="call-btn">
+            <section id="call-btn" data-aos="fade-right">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -598,12 +623,12 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
 
 
             
-<section id='BDC'>
+<section id='BDC' data-aos="fade-left">
   <nav aria-label="breadcrumb" class="breadcrumb-container">
     <ol class="breadcrumb">
     <li><Link to="/">Home</Link></li>
                     <li><Link to="/washing-machine-service">Washing machine service</Link></li>
-      <li class="active" aria-current="page">Haier Washing Machine Service</li>
+      <li class="active" aria-current="page">Haier Washing Machine Service Near Me</li>
     </ol>
   </nav>
 </section>
@@ -611,7 +636,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
 
 
 
-<section id='SDS'>
+<section id='SDS' data-aos="fade-right">
   <div className="container">
     <h2 className="sds-title">Haier Washing Machine Service – Complete Details</h2>
     <div className="sds-content">
@@ -621,7 +646,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
       <p><strong>Service Area:</strong> Madurai & Surrounding 20 km</p>
       <p><strong>Booking Mode:</strong> Online / Phone Call</p>
       <p><strong>Contact Number:</strong> <a href="tel:9025312399">9025312399</a></p>
-      <p><strong>Email:</strong> <a href="mailto:e2ewebservice@gmail.com">e2ewebservice@gmail.com</a></p>
+      <p><strong>Email:</strong> <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a></p>
       <p><strong>Website:</strong> <a href="https://servicee2e.in">https://servicee2e.in</a></p>
       <p><strong>Working Hours:</strong> 9 AM – 9 PM (All Days)</p>
       <p><strong>Home Visit:</strong> Yes, Doorstep Service Available</p>
@@ -636,7 +661,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
 
 
 
-<section id='faqs'>
+<section id='faqs' data-aos="fade-left">
   <div className="container">
     <h2 className="faq-title">Haier Washing Machine Service – FAQs</h2>
     <table className="faq-table">
@@ -695,7 +720,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
 
 
 
-<section id='UST'>
+<section id='UST' data-aos="fade-right">
 <div className='container'>
 <div className='row'>
 <div className='col-lg-12 col-md-12 col-12 col-sm-12'>
@@ -733,7 +758,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="interl">
+    <section id="interl" data-aos="fade-left">
     <div className='container'>
     <div className='row'>
       <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -761,7 +786,7 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     
     
     
-    <section id="FW">
+    <section id="FW" data-aos="fade-right">
         <div className="icon-container">
           <div className="icon facebook">
             <a
@@ -1002,30 +1027,30 @@ export default function HAIERWASHINGMACHINESERVICENEARME() {
     </section> */}
     
     
-      <section id="TABOFCONTENT">
+      <section id="TABOFCONTENT" data-aos="fade-right">
       <div className='container'>
         <h2>Table of Contents</h2>
         <ul id="TAB">
-          <li><a href="#headline">1. haier washing Machine Service Customer Care</a></li>
-          <li><a href="#hero">2. Hero Section</a></li>
-          <li><a href="#cchl">3. Customer Care Helpline</a></li>
-          <li><a href="#CEE">4. Contact Us</a></li>
-          <li><a href="#WRHY">5. We Are Ready to Help You</a></li>
-          <li><a href="#LIST">6. Customer Care Helpline Support</a></li>
-          <li><a href="#WASHMANSIT">7. Why Choose Us?</a></li>
-          <li><a href="#STARS">8. Customer Support Ratings</a></li>
-          <li><a href="#RATING">9. Customer Testimonials</a></li>
-          <li><a href="#ours">10. Our Service</a></li>
-          <li><a href="#DESCWASH">11. haier washing Machine Customer Support Service</a></li>
-          <li><a href="#WWO">12. What We Offer</a></li>
-          <li><a href="#interl">13. Related Services</a></li>
-          <li><a href="#FW">14. Follow Us</a></li>
+          {/* <li><a href="#headline">1. haier washing Machine Service Customer Care</a></li> */}
+          <li><a href="#hero">1. Hero Section</a></li>
+          <li><a href="#cchl">2. Customer Care Helpline</a></li>
+          <li><a href="#CEE">3. Contact Us</a></li>
+          <li><a href="#WRHY">4. We Are Ready to Help You</a></li>
+          <li><a href="#LIST">5. Customer Care Helpline Support</a></li>
+          <li><a href="#WASHMANSIT">6. Why Choose Us?</a></li>
+          <li><a href="#STARS">7. Customer Support Ratings</a></li>
+          <li><a href="#RATING">8. Customer Testimonials</a></li>
+          <li><a href="#ours">9. Our Service</a></li>
+          <li><a href="#DESCWASH">10. haier washing Machine Customer Support Service</a></li>
+          <li><a href="#WWO">11. What We Offer</a></li>
+          <li><a href="#interl">12. Related Services</a></li>
+          <li><a href="#FW">13. Follow Us</a></li>
         </ul>
       </div>
     </section>
     
     
-    <div><Footer/></div>
+    <div><Footers/></div>
     
              </>
   )
