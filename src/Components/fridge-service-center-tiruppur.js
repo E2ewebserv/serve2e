edd018f6202a7bef3182dbd70e929e0d57aa3edd
E2ewebserv <e2ewebservice@gmail.com>
2025-09-17 11:59:30 +0530

    Update  lavanya fridge-service-center-tiruppur.js

--- src/Components/fridge-service-center-tiruppur.js
+++ src/Components/fridge-service-center-tiruppur.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Header&Footer/Navbar';
-import Footer from '../Header&Footer/Footer';
+import Footers from '../Header&Footer/Footers';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
@@ -13,7 +13,8 @@ import EMAILPNG from '../images/EMAILPNG.png'
 import WHATSAPPPNG from '../images/WHATSAPPPNG.png'
 import { Helmet } from "react-helmet";
 // import FRIDGESERVICE from './fridge-service'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 export default function FRIDGESERVICECENTERTIRUPPUR() {
@@ -166,7 +167,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
               <Navbar />
             </div>
       
-      <section id="headline">
+      {/* <section id="headline">
             <div className='container'>
               <div className='row'>
                 <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -174,18 +175,41 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
                 </div>
               </div>
             </div>
-          </section>
-      
+          </section> */}
+      <section className="highlight-strip">
+  <div className="scroll-container">
+    <div className="scroll-track">
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  </div>
+</section>
             <section id="hero" style={{ backgroundImage: `url(${FRIDGEHERO})` }}>
                 <div className="hero-content">
-                  <h1>WE ARE READY TO HELP</h1>
+                  <h1>FRIDGE SERVICE CENTER TIRUPPUR</h1>
                   <h3>24*7 Service Support</h3>
                   
                   <a href="+919025312399" className="hero-btn">+919025312399</a>
                 </div>
               </section>
       
-      <section id="cchl">
+      <section id="cchl" data-aos="fade-right">
         <div className='container'>
   <div className='row'>
   <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -201,7 +225,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-        <section id="start">
+        <section id="start" data-aos="fade-left">
     <div className="container">
       <div className="row">
         <div className="col-12 text-center">
@@ -215,7 +239,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
     </div>
   </section>
   
-  <section id="CEE">
+  <section id="CEE" data-aos="fade-right">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
@@ -226,7 +250,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
         <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
           <h3>Email Us</h3>
           <img src={EMAILPNG} id="EMAILPNG" />
-          <a href="#">BOOK NOW</a>
+          <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a>
         </div>
         <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
           <h3>Enquiry Us</h3>
@@ -242,7 +266,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-  <section id="WRHY">
+  <section id="WRHY" data-aos="fade-left">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -255,12 +279,12 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </section>
   
   
-  <section id="LIST">
+  <section id="LIST" data-aos="fade-right">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
   
-          <h2> Fridge Customer Care Helpline Support</h2>
+          <h2 data-aos="fade-left"> Fridge Customer Care Helpline Support</h2>
           <ul>
          <li> <i class='fas fa-arrow-circle-right'></i>Book Your Complaints 24*7 Customer Care</li>
             <li> <i class='fas fa-arrow-circle-right'></i>Quick Service Within 2 Hours</li>
@@ -270,7 +294,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
             <li> <i class='fas fa-arrow-circle-right'></i>Trusted Certified Technicians</li>
           </ul>
   
-          <h2>Issues In Fridge Service</h2>
+          <h2 data-aos="fade-left">Issues In Fridge Service</h2>
           <ul>
             <li> <i class='fas fa-arrow-circle-right'></i>Refrigerator On-Off Problem</li>
             <li> <i class='fas fa-arrow-circle-right'></i>Noise in The Refrigerator</li>
@@ -281,7 +305,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
             <li> <i class='fas fa-arrow-circle-right'></i>Door Not Lock Problem</li>
           </ul>
   
-          <h2>Types Of Fridge Service</h2>
+          <h2 data-aos="fade-left">Types Of Fridge Service</h2>
           <ul>
             <li> <i class='fas fa-arrow-circle-right'></i>Small Fridge Service</li>
             <li> <i class='fas fa-arrow-circle-right'></i>Single Door Fridge Service</li>
@@ -295,7 +319,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </section>
   
   
-  <section id="WRHY">
+  <section id="WRHY" data-aos="fade-right">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -306,7 +330,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </div>
   </div>
   </section>
-  <section id="WASHMANSIT">
+  <section id="WASHMANSIT" data-aos="fade-left">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -333,7 +357,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </div>
   </div>
   </section> */}
-  <section id="STARS">
+  <section id="STARS" data-aos="fade-right">
     <div className='container'>
       <div className='row'>
        
@@ -363,7 +387,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
  
   
-  <section id="WRHY">
+  <section id="WRHY" data-aos="fade-left">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -376,7 +400,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </section>
   
   
-  <section id="RATING">
+  <section id="RATING" data-aos="fade-left">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-3 col-md-3 col-sm-12 col-12' id="borders">
@@ -495,7 +519,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-  <section id="ours">
+  <section id="ours" data-aos="fade-right">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -510,7 +534,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-  <section id="DESCWASH">
+  <section id="DESCWASH" data-aos="fade-left">
     <div className='container'>
       <div className='row'>
         <div className='col-lg-6 col-md-6 col-sm-12 col-12'>
@@ -540,7 +564,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   </section>
   
   
-  <section id="call-btn">
+  <section id="call-btn" data-aos="fade-right">
             <div className='container'>
               <div className='row'>
                 <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -555,7 +579,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-          <section id="WWO">
+          <section id="WWO" data-aos="fade-left">
             <div className='container'>
               <div className='row'>
                 <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -570,7 +594,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
             </div>
           </section>
   
-          <section id="call-btn">
+          <section id="call-btn" data-aos="fade-right">
             <div className='container'>
               <div className='row'>
                 <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -584,7 +608,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
 
 
-<section id='BDC'>
+<section id='BDC' data-aos="fade-left">
   <nav aria-label="breadcrumb" class="breadcrumb-container">
     <ol class="breadcrumb">
        <li><Link to="/">Home</Link></li>
@@ -597,7 +621,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
 
 
 
-<section id='SDS'>
+<section id='SDS' data-aos="fade-right">
   <div className="container">  
     <h2 className="sds-title">Fridge Service Center Tiruppur – Complete Details</h2>
     <div className="sds-content">
@@ -607,7 +631,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
       <p><strong>Service Area:</strong> tiruppur & Surrounding 20 km</p>
       <p><strong>Booking Mode:</strong> Online / Phone Call</p>
       <p><strong>Contact Number:</strong> <a href="tel:9025312399">9025312399</a></p>
-      <p><strong>Email:</strong> <a href="mailto:e2ewebservice@gmail.com">e2ewebservice@gmail.com</a></p>
+      <p><strong>Email:</strong> <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a></p>
       <p><strong>Website:</strong> <a href="https://servicee2e.in">https://servicee2e.in</a></p>
       <p><strong>Working Hours:</strong> 9 AM – 9 PM (All Days)</p>
       <p><strong>Home Visit:</strong> Yes, Doorstep Service Available</p>
@@ -619,7 +643,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
 
 
 
-<section id='faqs'>
+<section id='faqs' data-aos="fade-left">
   <div className="container">
     <h2 className="faq-title">fridge Service Center Tiruppur – FAQs</h2>
     <table className="faq-table">
@@ -676,7 +700,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
 
 
 
-<section id='UST'>
+<section id='UST' data-aos="fade-right">
   <div className='container'>
     <div className='row'>
       <div className='col-lg-12 col-md-12 col-12 col-sm-12'>
@@ -705,7 +729,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
 
 
   
-  <section id="interl">
+  <section id="interl" data-aos="fade-left">
   <div className='container'>
   <div className='row'>
     <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -733,7 +757,7 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
   
   
   
-  <section id="FW">
+  <section id="FW" data-aos="fade-right">
       <div className="icon-container">
         <div className="icon facebook">
           <a
@@ -759,31 +783,33 @@ export default function FRIDGESERVICECENTERTIRUPPUR() {
     </section>
   
   
+
   
-    <section id="TABOFCONTENT">
+  
+    <section id="TABOFCONTENT" data-aos="fade-left">
  <div className='container'>
    <h2>Table of Contents</h2>
    <ul id="TAB">
-     <li><a href="#headline">1. Fridge Service Customer Care</a></li>
-     <li><a href="#hero">2. Hero Section</a></li>
-     <li><a href="#cchl">3. Customer Care Helpline</a></li>
-     <li><a href="#CEE">4. Contact Us</a></li>
-     <li><a href="#WRHY">5. We Are Ready to Help You</a></li>
-     <li><a href="#LIST">6. Customer Care Helpline Support</a></li>
-     <li><a href="#WASHMANSIT">7. Why Choose Us?</a></li>
-     <li><a href="#STARS">8. Customer Support Ratings</a></li>
-     <li><a href="#RATING">9. Customer Testimonials</a></li>
-     <li><a href="#ours">10. Our Service</a></li>
-     <li><a href="#DESCWASH">11. Refrigerator Customer Support Service</a></li>
-     <li><a href="#WWO">12. What We Offer</a></li>
-     <li><a href="#interl">13. Related Services</a></li>
-     <li><a href="#FW">14. Follow Us</a></li>
+     {/* <li><a href="#headline">1. Fridge Service Customer Care</a></li> */}
+     <li><a href="#hero">1. Hero Section</a></li>
+     <li><a href="#cchl">2. Customer Care Helpline</a></li>
+     <li><a href="#CEE">3. Contact Us</a></li>
+     <li><a href="#WRHY">4. We Are Ready to Help You</a></li>
+     <li><a href="#LIST">5. Customer Care Helpline Support</a></li>
+     <li><a href="#WASHMANSIT">6. Why Choose Us?</a></li>
+     <li><a href="#STARS">7. Customer Support Ratings</a></li>
+     <li><a href="#RATING">8. Customer Testimonials</a></li>
+     <li><a href="#ours">9. Our Service</a></li>
+     <li><a href="#DESCWASH">10. Refrigerator Customer Support Service</a></li>
+     <li><a href="#WWO">11. What We Offer</a></li>
+     <li><a href="#interl">12. Related Services</a></li>
+     <li><a href="#FW">13. Follow Us</a></li>
    </ul>
  </div>
 </section>
   
   
-  <div><Footer/></div>
+  <div><Footers/></div>
   
            </>
   )
