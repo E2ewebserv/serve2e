e7739ca008c3b0655584832c0896d5b91c61c423
E2ewebserv <e2ewebservice@gmail.com>
2025-09-18 16:01:26 +0530

    Update lavanya led-tv-service-center-in-madurai.js

--- src/Components/led-tv-service-center-in-madurai.js
+++ src/Components/led-tv-service-center-in-madurai.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Header&Footer/Navbar';
-import Footer from '../Header&Footer/Footer';
+import Footers from '../Header&Footer/Footers';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
@@ -13,7 +13,8 @@ import EMAILPNG from '../images/EMAILPNG.png'
 import TVTECH from '../images/TVTECH.jpg'
 import WHATSAPPPNG from '../images/WHATSAPPPNG.png'
 // import LEDTVSERVICEINMADURAI from './led-tv-service-in-madurai'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 export default function LEDTVSERVICECENTERINMADURAI() {
@@ -103,7 +104,7 @@ useEffect(() => {
                            <Navbar />
                          </div>
                    
-                   <section id="headline">
+                   {/* <section id="headline">
                          <div className='container'>
                            <div className='row'>
                              <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -111,9 +112,7 @@ useEffect(() => {
                              </div>
                            </div>
                          </div>
-                       </section>
-
-
+                       </section> */}
          <section className="highlight-strip">
   <div className="scroll-container" direction="left">
     <div className="scroll-track">
@@ -142,14 +141,14 @@ useEffect(() => {
                    
                          <section id="hero" style={{ backgroundImage: `url(${TVHERO})` }}>
                              <div className="hero-content">
-                               <h1>WE ARE READY TO HELP</h1>
+                               <h1>LED TV SERVICE CENTER IN MADURAI</h1>
                                <h3>24*7 Service Support</h3>
                                
                                <a href="+919629398411" className="hero-btn">+919629398411</a>
                              </div>
                            </section>
                    
-                   <section id="cchl">
+                   <section id="cchl" data-aos="fade-left">
                      <div className='container'>
                <div className='row'>
                <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -165,7 +164,7 @@ useEffect(() => {
                
                
                
-                     <section id="start">
+                     <section id="start" data-aos="fade-right">
                  <div className="container">
                    <div className="row">
                      <div className="col-12 text-center">
@@ -179,7 +178,7 @@ useEffect(() => {
                  </div>
                </section>
                
-               <section id="CEE">
+               <section id="CEE" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
@@ -190,7 +189,7 @@ useEffect(() => {
                      <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
                        <h3>Email Us</h3>
                        <img src={EMAILPNG} id="EMAILPNG" />
-                       <a href="#">BOOK NOW</a>
+                       <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a>
                      </div>
                      <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
                        <h3>Enquiry Us</h3>
@@ -206,7 +205,7 @@ useEffect(() => {
                
                
                
-               <section id="WRHY">
+               <section id="WRHY" data-aos="fade-right">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -219,12 +218,12 @@ useEffect(() => {
                </section>
                
                
-               <section id="LIST">
+               <section id="LIST" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
                
-                       <h2>Customer Care Helpline Support</h2>
+                       <h2 data-aos="fade-right">Customer Care Helpline Support</h2>
                        <ul>
                       <li> <i class='fas fa-arrow-circle-right'></i>Book Your Complaints 24*7 Customer Care</li>
                          <li> <i class='fas fa-arrow-circle-right'></i>Quick Service Within 2 Hours</li>
@@ -234,7 +233,7 @@ useEffect(() => {
                          <li> <i class='fas fa-arrow-circle-right'></i>Trusted Certified Technicians</li>
                        </ul>
                
-                       <h2>Issues In TV</h2>
+                       <h2 data-aos="fade-right">Issues In TV</h2>
                        <ul>
                          <li> <i class='fas fa-arrow-circle-right'></i>LED TV On-Off Service</li>
                          <li> <i class='fas fa-arrow-circle-right'></i>Noise in The LED TV</li>
@@ -245,7 +244,7 @@ useEffect(() => {
                          <li> <i class='fas fa-arrow-circle-right'></i>LED TV Installation</li>
                        </ul>
                
-                       <h2>Types Of TV</h2>
+                       <h2 data-aos="fade-right">Types Of TV</h2>
                        <ul>
                          <li> <i class='fas fa-arrow-circle-right'></i>LED TV Service</li>
                          <li> <i class='fas fa-arrow-circle-right'></i>LCD TV Service</li>
@@ -259,7 +258,7 @@ useEffect(() => {
                </section>
                
                
-               <section id="WRHY">
+               <section id="WRHY" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -271,7 +270,7 @@ useEffect(() => {
                </div>
                
                </section>
-               <section id="WASHMANSIT">
+               <section id="WASHMANSIT" data-aos="fade-right">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -298,7 +297,7 @@ useEffect(() => {
                </div>
                </div>
                </section> */}
-               <section id="STARS">
+               <section id="STARS" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                     
@@ -337,7 +336,7 @@ useEffect(() => {
                
                
                
-               <section id="WRHY">
+               <section id="WRHY" data-aos="fade-right">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -350,7 +349,7 @@ useEffect(() => {
                </section>
                
                
-               <section id="RATING">
+               <section id="RATING" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-3 col-md-3 col-sm-12 col-12' id="borders">
@@ -469,7 +468,7 @@ useEffect(() => {
                
                
                
-               <section id="ours">
+               <section id="ours" data-aos="fade-right"> 
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -484,7 +483,7 @@ useEffect(() => {
                
                
                
-               <section id="DESCWASH">
+               <section id="DESCWASH" data-aos="fade-left">
                  <div className='container'>
                    <div className='row'>
                      <div className='col-lg-6 col-md-6 col-sm-12 col-12'>
@@ -520,7 +519,7 @@ useEffect(() => {
                </section>
                
                
-               <section id="call-btn">
+               <section id="call-btn" data-aos="fade-right">
                          <div className='container'>
                            <div className='row'>
                              <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -535,7 +534,7 @@ useEffect(() => {
                
                
                
-                       <section id="WWO">
+                       <section id="WWO" data-aos="fade-left">
                          <div className='container'>
                            <div className='row'>
                              <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -550,7 +549,7 @@ useEffect(() => {
                          </div>
                        </section>
                
-                       <section id="call-btn">
+                       <section id="call-btn" data-aos="fade-right">
                          <div className='container'>
                            <div className='row'>
                              <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -565,7 +564,7 @@ useEffect(() => {
 
 
 
-<section id='BDC'>
+<section id='BDC' data-aos="fade-left">
   <nav aria-label="breadcrumb" class="breadcrumb-container">
     <ol class="breadcrumb">
       <li><Link to="/">Home</Link></li>
@@ -578,7 +577,7 @@ useEffect(() => {
 
 
 
-<section id='SDS'>
+<section id='SDS' data-aos="fade-right">
   <div className="container">
     <h2 className="sds-title">LED Tv Service – Complete Details</h2>
     <div className="sds-content">
@@ -588,7 +587,7 @@ useEffect(() => {
 <p><strong>Service Area:</strong> Madurai & Surrounding 20 km</p>
 <p><strong>Booking Mode:</strong> Online / Phone Call</p>
 <p><strong>Contact Number:</strong> <a href="tel:9025312399">9025312399</a></p>
-<p><strong>Email:</strong> <a href="mailto:e2ewebservice@gmail.com">e2ewebservice@gmail.com</a></p>
+<p><strong>Email:</strong> <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a></p>
 <p><strong>Website:</strong> <a href="https://servicee2e.in">https://servicee2e.in</a></p>
 <p><strong>Working Hours:</strong> 9 AM – 9 PM (All Days)</p>
 <p><strong>Home Visit:</strong> Yes, Doorstep Service Available</p>
@@ -603,7 +602,7 @@ useEffect(() => {
 
 
 
-<section id="faqs">
+<section id="faqs" data-aos="fade-left">
   <div className="container">
     <h2 className="faq-title">LED TV Service – FAQs</h2>
     <table className="faq-table">
@@ -696,7 +695,7 @@ useEffect(() => {
 
 
 
-<section id='UST'>
+<section id='UST' data-aos="fade-right">
 <div className='container'>
 <div className='row'>
 <div className='col-lg-12 col-md-12 col-12 col-sm-12'>
@@ -728,7 +727,7 @@ useEffect(() => {
 
 
                
-               <section id="interl">
+               <section id="interl" data-aos="fade-left"> 
                <div className='container'>
                <div className='row'>
                  <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -756,7 +755,7 @@ useEffect(() => {
                
                
                
-               <section id="FW">
+               <section id="FW" data-aos="fade-right">
                    <div className="icon-container">
                      <div className="icon facebook">
                        <a
@@ -783,31 +782,31 @@ useEffect(() => {
                
                
                
-                 <section id="TABOFCONTENT">
+                 <section id="TABOFCONTENT" data-aos="fade-left">
           <div class="container">
             <h2>Table of Contents</h2>
             <ul id="TAB">
-              <li><a href="#headline">1. TV Service Customer Care</a></li>
-              <li><a href="#hero">2. Hero Section</a></li>
-              <li><a href="#cchl">3. Customer Care Helpline</a></li>
-              <li><a href="#CEE">4. Contact Us</a></li>
-              <li><a href="#WRHY">5. We Are Ready to Help You</a></li>
-              <li><a href="#LIST">6. Customer Care Helpline Support</a></li>
-              <li><a href="#WASHMANSIT">7. Why Choose Us?</a></li>
-              <li><a href="#STARS">8. Customer Support Ratings</a></li>
-              <li><a href="#RATING">9. Customer Testimonials</a></li>
-              <li><a href="#ours">10. Our Service</a></li>
-              <li><a href="#DESCWASH">11. TV Customer Support Service</a></li>
-              <li><a href="#WWO">12. What We Offer</a></li>
-              <li><a href="#interl">13. Related Services</a></li>
-              <li><a href="#FW">14. Follow Us</a></li>
+              {/* <li><a href="#headline">1. TV Service Customer Care</a></li> */}
+              <li><a href="#hero">1. Hero Section</a></li>
+              <li><a href="#cchl">2. Customer Care Helpline</a></li>
+              <li><a href="#CEE">3. Contact Us</a></li>
+              <li><a href="#WRHY">4. We Are Ready to Help You</a></li>
+              <li><a href="#LIST">5. Customer Care Helpline Support</a></li>
+              <li><a href="#WASHMANSIT">6. Why Choose Us?</a></li>
+              <li><a href="#STARS">7. Customer Support Ratings</a></li>
+              <li><a href="#RATING">8. Customer Testimonials</a></li>
+              <li><a href="#ours">9. Our Service</a></li>
+              <li><a href="#DESCWASH">10. TV Customer Support Service</a></li>
+              <li><a href="#WWO">11. What We Offer</a></li>
+              <li><a href="#interl">12. Related Services</a></li>
+              <li><a href="#FW">13. Follow Us</a></li>
             </ul>
           </div>
         </section>
         
                
                
-               <div><Footer/></div>
+               <div><Footers/></div>
                
                         </>
   );
