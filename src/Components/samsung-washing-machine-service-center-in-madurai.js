257bb1e46c0787bedef66a1292be31f3d5b6de47
E2ewebserv <e2ewebservice@gmail.com>
2025-09-19 16:25:12 +0530

    Update lavanya samsung-washing-machine-service-center-in-madurai.js

--- src/Components/samsung-washing-machine-service-center-in-madurai.js
+++ src/Components/samsung-washing-machine-service-center-in-madurai.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Header&Footer/Navbar';
-import Footer from '../Header&Footer/Footer';
+import Footers from '../Header&Footer/Footers';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
@@ -12,7 +12,8 @@ import WHATSAPPPNG from '../images/WHATSAPPPNG.png'
 import WASHSIT from '../images/WASHSIT.jpg'
 import WASHDESC from '../images/WASHDESC.webp'
 import { Helmet } from "react-helmet";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
   useEffect(() => {
     window.scrollTo(0, 0); // Scroll to the top of the page
@@ -165,7 +166,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
                 <Navbar />
               </div>
         
-        <section id="headline">
+        {/* <section id="headline">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -173,19 +174,42 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
                   </div>
                 </div>
               </div>
-            </section>
-        
+            </section> */}
+        <section className="highlight-strip">
+  <div className="scroll-container">
+    <div className="scroll-track">
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="scroll-content">
+        🚚 Free Visit Within 5 KM &nbsp;&nbsp;&nbsp;
+        🛠️ Up to 1 Year Spare Warranty &nbsp;&nbsp;&nbsp;
+        🧰 Transparent Spare Parts Pricing &nbsp;&nbsp;&nbsp;
+        🔁 30 Days Money Back If Not Satisfied * &nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  </div>
+</section>
               <section id="hero" style={{ backgroundImage: `url(${WASHERHERO})` }}>
                   <div className="hero-content">
                      
-                    <h1>WE ARE READY TO HELP</h1>
+                    <h1>SAMSUNG WASHING MACHINE SERVICE CENTER IN MADURAI</h1>
                     <h3>24*7 Samsung Service Support</h3>
                     
                     <a href="tel:+919025312399" className="hero-btn">+919025312399</a>
                   </div>
                 </section>
         
-        <section id="cchl">
+        <section id="cchl" data-aos="fade-right">
           <div className='container'>
     <div className='row'>
     <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -201,7 +225,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-          <section id="start">
+          <section id="start" data-aos="fade-left">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
@@ -215,7 +239,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
       </div>
     </section>
     
-    <section id="CEE">
+    <section id="CEE" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
@@ -226,7 +250,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Email Us</h3>
             <img src={EMAILPNG} id="EMAILPNG" />
-            <a href="#">BOOK NOW</a>
+             <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a>
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <h3>Enquiry Us</h3>
@@ -242,7 +266,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -255,12 +279,12 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </section>
     
     
-    <section id="LIST">
+    <section id="LIST" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
     
-            <h2> Samsung Customer Care Helpline Support</h2>
+            <h2 data-aos="fade-left"> Samsung Customer Care Helpline Support</h2>
             <ul>
            <li> <i class='fas fa-arrow-circle-right'></i>Book Your Complaints 24*7 Customer Care</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Quick Service Within 2 Hours</li>
@@ -270,7 +294,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
               <li> <i class='fas fa-arrow-circle-right'></i>Trusted Certified Technicians</li>
             </ul>
     
-            <h2>Issues In Washing Machine</h2>
+            <h2 data-aos="fade-left">Issues In Washing Machine</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Washing Machine On-Off Problem</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Noise in The Washing Machine</li>
@@ -281,7 +305,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
               <li> <i class='fas fa-arrow-circle-right'></i>Door Not Lock Problem</li>
             </ul>
     
-            <h2>Types Of Samsung Washing Machine Service</h2>
+            <h2 data-aos="fade-left">Types Of Samsung Washing Machine Service</h2>
             <ul>
               <li> <i class='fas fa-arrow-circle-right'></i>Top Load Washing Machine Service</li>
               <li> <i class='fas fa-arrow-circle-right'></i>Front Load Washing Machine Service</li>
@@ -294,7 +318,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </section>
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -305,7 +329,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </div>
     </div>
     </section>
-    <section id="WASHMANSIT">
+    <section id="WASHMANSIT" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -332,7 +356,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </div>
     </div>
     </section> */}
-    <section id="STARS">
+    <section id="STARS" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
          
@@ -371,7 +395,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-    <section id="WRHY">
+    <section id="WRHY" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -384,7 +408,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </section>
     
     
-    <section id="RATING">
+    <section id="RATING" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-3 col-md-3 col-sm-12 col-12' id="borders">
@@ -497,7 +521,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-    <section id="ours">
+    <section id="ours" data-aos="fade-left">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -512,7 +536,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-    <section id="DESCWASH">
+    <section id="DESCWASH" data-aos="fade-right">
       <div className='container'>
         <div className='row'>
           <div className='col-lg-6 col-md-6 col-sm-12 col-12'>
@@ -539,7 +563,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     </section>
     
     
-    <section id="call-btn">
+    <section id="call-btn" data-aos="fade-left">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -554,7 +578,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-            <section id="WWO">
+            <section id="WWO" data-aos="fade-right">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -569,7 +593,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
               </div>
             </section>
     
-            <section id="call-btn">
+            <section id="call-btn" data-aos="fade-left">
               <div className='container'>
                 <div className='row'>
                   <div className='col-lg-12 colmd-12 col-sm-12 col-12'>
@@ -582,12 +606,12 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
             </section>
     
     
-<section id='BDC'>
+<section id='BDC' data-aos="fade-right">
   <nav aria-label="breadcrumb" class="breadcrumb-container">
     <ol class="breadcrumb">
       <li><Link to="/">Home</Link></li>
   <li><Link to="/washing-machine-service">Washing machine service</Link></li>
-      <li class="active" aria-current="page">samsung Washing Machine Service</li>
+      <li class="active" aria-current="page">samsung Washing Machine Service Center In Madurai</li>
     </ol>
   </nav>
 </section>
@@ -595,7 +619,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
 
 
 
-<section id='SDS'>
+<section id='SDS' data-aos="fade-left">
   <div className="container">
     <h2 className="sds-title">Samsung Washing Machine Service – Complete Details</h2>
     <div className="sds-content">
@@ -605,7 +629,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
       <p><strong>Service Area:</strong> Madurai & Surrounding 20 km</p>
       <p><strong>Booking Mode:</strong> Online / Phone Call</p>
       <p><strong>Contact Number:</strong> <a href="tel:9025312399">9025312399</a></p>
-      <p><strong>Email:</strong> <a href="mailto:e2ewebservice@gmail.com">e2ewebservice@gmail.com</a></p>
+      <p><strong>Email:</strong>  <a href="mailto:expresshelplinecare@gmail.com">expresshelplinecare@gmail.com</a></p>
       <p><strong>Website:</strong> <a href="https://servicee2e.in">https://servicee2e.in</a></p>
       <p><strong>Working Hours:</strong> 9 AM – 9 PM (All Days)</p>
       <p><strong>Home Visit:</strong> Yes, Doorstep Service Available</p>
@@ -620,7 +644,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
 
 
 
-<section id='faqs'>
+<section id='faqs' data-aos="fade-right">
   <div className="container">
     <h2 className="faq-title">Samsung Washing Machine Service – FAQs</h2>
     <table className="faq-table">
@@ -677,7 +701,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
 
 
 
-<section id='UST'>
+<section id='UST' data-aos="fade-left">
 <div className='container'>
 <div className='row'>
 <div className='col-lg-12 col-md-12 col-12 col-sm-12'>
@@ -712,7 +736,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
 
 
 
-    <section id="interl">
+    <section id="interl" data-aos="fade-right">
     <div className='container'>
     <div className='row'>
       <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
@@ -740,7 +764,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
     
     
-    <section id="FW">
+    <section id="FW" data-aos="fade-left">
         <div className="icon-container">
           <div className="icon facebook">
             <a
@@ -767,7 +791,7 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
     
  
  
-    {/* <section id="area">
+    <section id="area" data-aos="fade-right">
  <div className='container'>
      <div className='row'>
          <center>
@@ -978,33 +1002,33 @@ export default function SAMSUNGWASHINGMACINESERVICECENTERINMADURAI() {
  </div>
  
  
-    </section> */}
+    </section> 
     
     
-      <section id="TABOFCONTENT">
+      <section id="TABOFCONTENT" data-aos="fade-right">
       <div className='container'>
         <h2>Table of Contents</h2>
         <ul id="TAB">
-          <li><a href="#headline">1. Washing Machine Service Customer Care</a></li>
-          <li><a href="#hero">2. Hero Section</a></li>
-          <li><a href="#cchl">3. Customer Care Helpline</a></li>
-          <li><a href="#CEE">4. Contact Us</a></li>
-          <li><a href="#WRHY">5. We Are Ready to Help You</a></li>
-          <li><a href="#LIST">6. Customer Care Helpline Support</a></li>
-          <li><a href="#WASHMANSIT">7. Why Choose Us?</a></li>
-          <li><a href="#STARS">8. Customer Support Ratings</a></li>
-          <li><a href="#RATING">9. Customer Testimonials</a></li>
-          <li><a href="#ours">10. Our Service</a></li>
-          <li><a href="#DESCWASH">11. Washing Machine Customer Support Service</a></li>
-          <li><a href="#WWO">12. What We Offer</a></li>
-          <li><a href="#interl">13. Related Services</a></li>
-          <li><a href="#FW">14. Follow Us</a></li>
+          {/* <li><a href="#headline">1. Washing Machine Service Customer Care</a></li> */}
+          <li><a href="#hero">1. Hero Section</a></li>
+          <li><a href="#cchl">2. Customer Care Helpline</a></li>
+          <li><a href="#CEE">3. Contact Us</a></li>
+          <li><a href="#WRHY">4. We Are Ready to Help You</a></li>
+          <li><a href="#LIST">5. Customer Care Helpline Support</a></li>
+          <li><a href="#WASHMANSIT">6. Why Choose Us?</a></li>
+          <li><a href="#STARS">7. Customer Support Ratings</a></li>
+          <li><a href="#RATING">8. Customer Testimonials</a></li>
+          <li><a href="#ours">9. Our Service</a></li>
+          <li><a href="#DESCWASH">10. Washing Machine Customer Support Service</a></li>
+          <li><a href="#WWO">11. What We Offer</a></li>
+          <li><a href="#interl">12. Related Services</a></li>
+          <li><a href="#FW">13. Follow Us</a></li>
         </ul>
       </div>
     </section>
     
     
-    <div><Footer/></div>
+    <div><Footers/></div>
     
              </>
   )
